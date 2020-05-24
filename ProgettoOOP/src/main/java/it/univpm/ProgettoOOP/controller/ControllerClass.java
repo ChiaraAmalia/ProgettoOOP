881216b12c0ad9c68ba17d968f06517a2a413f6d
Piero Campitelli <S1089287@studenti.univpm.it>
2020-05-24 20:26:36 +0200

    Inizio Controller e Database

--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/controller/ControllerClass.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/controller/ControllerClass.java
@@ -1,5 +1,20 @@
 package it.univpm.ProgettoOOP.controller;
 
+import java.util.ArrayList;
+
+/** Classe che si occupa di effettuare le chiamate al Server.
+ * @author Piero Campitelli
+ * @author Chiara Amalia Caporusso
+ */
+@RestController
 public class ControllerClass {
+/** Risponde alla richiesta GET /tweet 
+ * @return ArrayList di oggetti Tweet
+ */
+	@RequestMapping(valure="tweet",method=RequestMethod.GET)
+	public ArrayList<Tweet> getTweet(){
+		return DatabaseClass.getTweet;
+	}
+	
 
 }
