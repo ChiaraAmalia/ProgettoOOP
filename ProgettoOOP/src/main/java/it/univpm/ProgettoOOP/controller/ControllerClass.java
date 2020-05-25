13fa52ef2672694916c723ffc4ca5d51b5e5632b
Chiara Amalia Caporusso <s1087171@studenti.univpm.it>
2020-05-25 09:41:48 +0200

    corretti errori generali

--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/controller/ControllerClass.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/controller/ControllerClass.java
@@ -2,6 +2,13 @@ package it.univpm.ProgettoOOP.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.univpm.ProgettoOOP.database.DatabaseClass;
+import it.univpm.ProgettoOOP.model.Tweet;
+
 /** Classe che si occupa di effettuare le chiamate al Server.
  * @author Piero Campitelli
  * @author Chiara Amalia Caporusso
@@ -11,9 +18,9 @@ public class ControllerClass {
 /** Risponde alla richiesta GET /tweet 
  * @return ArrayList di oggetti Tweet
  */
-	@RequestMapping(valure="tweet",method=RequestMethod.GET)
+	@RequestMapping(value="tweet",method=RequestMethod.GET)
 	public ArrayList<Tweet> getTweet(){
-		return DatabaseClass.getTweet;
+		return DatabaseClass.getTweet();
 	}
 	
 
--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/database/DatabaseClass.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/database/DatabaseClass.java
@@ -1,4 +1,8 @@
-package database;
+package it.univpm.ProgettoOOP.database;
+
+import java.util.ArrayList;
+
+import it.univpm.ProgettoOOP.model.Tweet;
 
 public class DatabaseClass {
 	public static ArrayList<Tweet> tweet= new ArrayList <Tweet>();
