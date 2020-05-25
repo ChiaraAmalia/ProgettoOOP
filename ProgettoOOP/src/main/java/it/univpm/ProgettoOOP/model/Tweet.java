aefba932310a938a620486d35b6d2c1487371e27
Chiara Amalia Caporusso <s1087171@studenti.univpm.it>
2020-05-25 12:53:15 +0200

    aggiunta e modifica di alcune classi

--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/model/Tweet.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/model/Tweet.java
@@ -2,13 +2,134 @@ package it.univpm.ProgettoOOP.model;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author Chiara Amalia Caporusso
+ * @author Piero Campitelli
+ *
+ */
+
 public class Tweet {
 	
-	protected String dataOra;
-	protected int idTweet;
-	protected String testo;
-	protected ArrayList<Entities> entities = new ArrayList<Entities>();
+	private String dataOra;
+	private int idTweet;
+	private String testo;
+	private ArrayList<Entities> entities = new ArrayList<Entities>();
+	private ArrayList<User> users = new ArrayList<User>();
+	private boolean retweeted;
+	
+	/**
+	 * 
+	 * @param dataOra
+	 * @param idTweet
+	 * @param testo
+	 * @param entities
+	 * @param users
+	 * 
+	 * the constructor
+	 */
+	public Tweet(String dataOra, int idTweet, String testo, ArrayList<Entities> entities,
+				ArrayList<User> users, boolean retweeted) {
+		
+		super();
+		this.dataOra = dataOra;
+		this.idTweet = idTweet;
+		this.testo = testo;
+		this.entities = entities;
+		this.users = users;
+		this.retweeted = retweeted;
+	}
 
+	/**
+	 * @return the dataOra
+	 */
+	public String getDataOra() {
+		return dataOra;
+	}
+
+	/**
+	 * @param dataOra the dataOra to set
+	 */
+	public void setDataOra(String dataOra) {
+		this.dataOra = dataOra;
+	}
+
+	/**
+	 * @return the idTweet
+	 */
+	public int getIdTweet() {
+		return idTweet;
+	}
+
+	/**
+	 * @param idTweet the idTweet to set
+	 */
+	public void setIdTweet(int idTweet) {
+		this.idTweet = idTweet;
+	}
+
+	/**
+	 * @return the testo
+	 */
+	public String getTesto() {
+		return testo;
+	}
+
+	/**
+	 * @param testo the testo to set
+	 */
+	public void setTesto(String testo) {
+		this.testo = testo;
+	}
+
+	/**
+	 * @return the entities
+	 */
+	public ArrayList<Entities> getEntities() {
+		return entities;
+	}
+
+	/**
+	 * @param entities the entities to set
+	 */
+	public void setEntities(ArrayList<Entities> entities) {
+		this.entities = entities;
+	}
+
+	/**
+	 * @return the users
+	 */
+	public ArrayList<User> getUsers() {
+		return users;
+	}
+
+	/**
+	 * @param users the users to set
+	 */
+	public void setUsers(ArrayList<User> users) {
+		this.users = users;
+	}
+	
+	/**
+	 * @return the retweeted
+	 */
+	public boolean isRetweeted() {
+		return retweeted;
+	}
+
+	/**
+	 * @param retweeted the retweeted to set
+	 */
+	public void setRetweeted(boolean retweeted) {
+		this.retweeted = retweeted;
+	}
+
+	@Override
+	public String toString() {
+		return "\nTweet[\n dataOra=" + dataOra + "\n idTweet=" + idTweet + "\n testo=" + testo + "\n entities=" + entities
+				+ "\n users=" + users + "\n retweeted" + retweeted + "\n ]";
+		
+	}
 	
 	
 }
--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/model/UserMentions.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/model/UserMentions.java
@@ -1,5 +1,66 @@
 package it.univpm.ProgettoOOP.model;
 
 public class UserMentions {
-
+	
+	private String ScreenName;
+	private String name;
+	private int id;
+	
+	/**
+	 * @param screenName
+	 * @param name
+	 * @param id
+	 * the constructor
+	 */
+	
+	public UserMentions(String screenName, String name, int id) {
+		super();
+		ScreenName = screenName;
+		this.name = name;
+		this.id = id;
+	}
+	/**
+	 * @return the screenName
+	 */
+	public String getScreenName() {
+		return ScreenName;
+	}
+	/**
+	 * @param screenName the screenName to set
+	 */
+	public void setScreenName(String screenName) {
+		ScreenName = screenName;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public String toString() {
+		return "\nUserMentions[\n ScreenName=" + ScreenName + "\n name=" + name + "\n id=" + id + "\n ]";
+	}
+	
+	
+	
 }
