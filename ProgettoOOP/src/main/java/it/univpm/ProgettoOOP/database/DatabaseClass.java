2bc83eac8040ccd1bea7014adeebe07b4902a2a5
Chiara Amalia Caporusso <s1087171@studenti.univpm.it>
2020-05-26 17:51:10 +0200

    modifica classe JsonParser recupero Parser

--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/database/DatabaseClass.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/database/DatabaseClass.java
@@ -53,7 +53,42 @@ public class DatabaseClass {
 
 	}
 	
-	
+	public static void InformazioniSalvate(JSONObject json) throws Exception {
+		
+		if(json != null) {
+				try {
+					JSONArray dataset = (JSONArray) json.get("timeline");
+					 int length =  (int) dataset.size();
+					if(dataset != null) {
+						Timeline timeline = new Timeline();
+							for(int i=0; i<length; i++) {
+									JSONObject tweet = (JSONObject) dataset.get(i);
+								
+									Tweet tw = new Tweet();
+								
+									tw.setDataOra((String) tweet.get("created_at"));
+									tw.setIdTweet((int) tweet.get("id"));
+									tw.setTesto((String)tweet.get("full_text"));
+									tw.setRetweeted((boolean)tweet.get("retweeted"));
+									
+									Hashtag hashtag;
+									JSONObject obj = (JSONObject) tweet.get("entities");
+									JSONArray Hashtag = (JSONArray) obj.get("hashtags");
+									
+									for(int j=0; j<Hashtag.size(); j++) {
+										hashtag = new Hashtag();
+										hashtag.getTesto();
+									}
+							}
+
+					}
+					
+				}catch (SecurityException e) {
+					 throw new Exception("Error in parsing I/O operation");
+				}
+
+		}
+	}
 	
 }
 	
--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/service/JsonParser.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/service/JsonParser.java
@@ -1,13 +1,17 @@
 package it.univpm.ProgettoOOP.service;
 
+import java.security.Security;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Collection;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ch.qos.logback.core.filter.Filter;
 import it.univpm.ProgettoOOP.model.Entities;
 import it.univpm.ProgettoOOP.model.Hashtag;
 import it.univpm.ProgettoOOP.model.Timeline;
@@ -15,41 +19,60 @@ import it.univpm.ProgettoOOP.model.Tweet;
 
 public class JsonParser {
 	
-	public static void ParseJson(JSONObject json) throws Exception {
-		
-		if(json != null) {
-				try {
-					JSONArray dataset = (JSONArray) json.get("timeline");
-					 int length =  (int) dataset.size();
-					if(dataset != null) {
-						Timeline timeline = new Timeline();
-							for(int i=0; i<length; i++) {
-									JSONObject tweet = (JSONObject) dataset.get(i);
-								
-									Tweet tw = new Tweet();
-								
-									tw.setDataOra((String) tweet.get("created_at"));
-									tw.setIdTweet((int) tweet.get("id"));
-									tw.setTesto((String)tweet.get("full_text"));
-									tw.setRetweeted((boolean)tweet.get("retweeted"));
-									
-									Hashtag hashtag;
-									JSONObject obj = (JSONObject) tweet.get("entities");
-									JSONArray Hashtag = (JSONArray) obj.get("hashtags");
-									
-									for(int j=0; j<Hashtag.size(); j++) {
-										hashtag = new Hashtag();
-										hashtag.getTesto();
-									}
-							}
+	
+	/**Effettua il parsing dei dati esterni (Campo da analizzare)
+	  @param filtro da applicare
+	  @return ArrayList di Tweet filtrati
+	 */
+	public static ArrayList<Tweet> JsonParserColumn(Object filter)
+			throws Exception{ 
+				ArrayList<Tweet> previousArray= new ArrayList<Tweet>();
+				ArrayList<Tweet> filteredArray= new ArrayList<Tweet>();
+			//vedere se posso anche non istanziarlo quì
+				HashMap<String,Object> result= new ObjectMapper().convertValue(filter,HashMap.class);
 
+			//Itera con tutti gli elementi dell'ArrayList
+				for(Map.Entry<String, Object> entry: result.entrySet()) {
+					filteredArray= new ArrayList<Tweet>();
+					Object filterParam=entry.getValue();
+					Object column=entry.getKey();
+					try {
+						filteredArray=jsonParserOperator(column,filterParam,previousArray);
+					}catch (SecurityException e) {
+						throw new Exception ("Error in I/O parsing information");
 					}
-					
-				}catch (SecurityException e) {
-					 throw new Exception("Error in parsing I/O operation");
 				}
+//				previousArray= new ArrayList<Tweet>();
+				previousArray.addAll(filteredArray);
+				return filteredArray;
+			}
+		
+	public static ArrayList<Tweet> jsonParserOperator (Object column,Object filterParam,ArrayList<Tweet> previousArray)
+			throws Exception{
+		String line=" ";
+		Filter filter;
+		ArrayList <Tweet> filteredArray= new ArrayList <Tweet>();
+		HashMap <String,Object> result= new ObjectMapper().convertValue(filterParam,HashMap.class);
 
-		}
-	}
+		for(Map.Entry<String, Object> entry: result.entrySet()) {
+			String operator=(String) entry.getValue();
+			Object value=entry.getValue();
+			if((operator.equals("type")) || (operator.contentEquals("Type"))) {
+				line=(String) value;
+				if((!(value.equals("and"))&&(!(value.equals("or"))))) {
+					throw new Exception("'and' o 'or' expected after 'type'");
+		    	}
+		    	continue;
+		    }
+			filter=FilterService.istanceFilter(column,operator,value);
+			if (line.equals("and"))
+				filteredArray = FilterService.runFilterAND(filter, previousArray);
+			else
+				filteredArray = FilterService.runFilterOR(filter, previousArray);
+		        }
+			
+				return filteredArray;
+				
+		    }
 }
 
