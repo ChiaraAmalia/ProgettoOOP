28cad96f95014fd8bf7da218aa598f5b8e940094
Chiara Amalia Caporusso <s1087171@studenti.univpm.it>
2020-06-04 16:02:07 +0200

    aggiustati filtri

--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/filter/FilterImageIncluded.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/filter/FilterImageIncluded.java
@@ -1,10 +1,22 @@
 package it.univpm.ProgettoOOP.filter;
 
+import it.univpm.ProgettoOOP.model.Tweet;
 import it.univpm.ProgettoOOP.other.Filter;
+import it.univpm.ProgettoOOP.other.Filter1Compare;
 
-public class FilterImageIncluded implements Filter {
-	public boolean filter1(Object image) {
-		if()
+public class FilterImageIncluded extends Filter1Compare implements Filter {
+	
+	public FilterImageIncluded(Object paramCompare) {
+		super(paramCompare);
+	}
+	
+	public boolean filter(Tweet tweet) {
+		
+		for(int i=0; i<tweet.getEntities().size(); i++) {
+			if(tweet.getEntities().get(i).getImages()!=null)
+				return true;
+		}
+		return false;
 	}
 
 }
--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/filter/FilterImageNotIncluded.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/filter/FilterImageNotIncluded.java
@@ -1,5 +1,20 @@
 package it.univpm.ProgettoOOP.filter;
 
-public class FilterImageNotIncluded {
+import it.univpm.ProgettoOOP.model.Tweet;
+import it.univpm.ProgettoOOP.other.Filter;
+import it.univpm.ProgettoOOP.other.Filter1Compare;
 
+public class FilterImageNotIncluded extends Filter1Compare implements Filter {
+
+	public FilterImageNotIncluded(Object paramCompare) {
+		super(paramCompare);
+	}
+	
+	public boolean filter(Tweet tweet) {
+		for(int i=0; i<tweet.getEntities().size(); i++) {
+			if(tweet.getEntities().get(i).getImages()==null)
+				return true;
+		}
+		return false;
+	}
 }
