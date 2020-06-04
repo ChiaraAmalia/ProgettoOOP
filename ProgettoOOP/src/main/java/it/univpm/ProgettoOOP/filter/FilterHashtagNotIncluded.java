9184026f13702fb24a35802d5f985954ecf3fc06
Chiara Amalia Caporusso <s1087171@studenti.univpm.it>
2020-06-04 15:17:27 +0200

    aggiustata altra classe filtro

--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/filter/FilterHashtagNotIncluded.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/filter/FilterHashtagNotIncluded.java
@@ -1,21 +1,22 @@
 package it.univpm.ProgettoOOP.filter;
 
-import com.fasterxml.classmate.Filter;
-
+import it.univpm.ProgettoOOP.other.Filter;
 import it.univpm.ProgettoOOP.model.Hashtag;
 import it.univpm.ProgettoOOP.model.Tweet;
 import it.univpm.ProgettoOOP.other.Filter1Compare;
 
-public class FilterHashtagIncluded extends Filter1Compare implements Filter{
+public class FilterHashtagNotIncluded extends Filter1Compare implements Filter{
 	
-	public FilterHashtagIncluded(Object paramCompare) {
+	public FilterHashtagNotIncluded(Object paramCompare) {
 		super(paramCompare);
 	}
 	
-	public boolean filter(Hashtag hashtag) {
-		Hashtag hashtags = new Hashtag();
-		if(!(hashtags.getText()).equals(hashtag.getText())) {
-			return true;
+	public boolean filter(Tweet tweet) {
+		for(int i=0; i<tweet.getEntities().size(); i++) {
+			for(int j=0; j<tweet.getEntities().get(i).getHashtags().size(); j++) {
+				if(!((tweet.getEntities().get(i).getHashtags().get(j).getText()).equals(tweet.getEntities().get(i).getHashtags().get(j).getText())))
+					return true;
+			}
 	}
 		return false;
 		
