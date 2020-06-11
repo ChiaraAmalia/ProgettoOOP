ae96b56a88656d43e4c93b5f4b3d9716c5929a46
Chiara Amalia Caporusso <s1087171@studenti.univpm.it>
2020-06-11 12:36:07 +0200

    test funzionanti

--- ProgettoOOP/src/test/java/it/univpm/ProgettoOOP/TestFilterService.java
+++ ProgettoOOP/src/test/java/it/univpm/ProgettoOOP/TestFilterService.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import it.univpm.ProgettoOOP.Exception.FilterIllegalArgumentException;
 import it.univpm.ProgettoOOP.Exception.FilterNotFoundException;
 import it.univpm.ProgettoOOP.service.FilterService;
 
@@ -28,6 +29,6 @@ public class TestFilterService {
 		assertThrows(FilterNotFoundException.class, ()-> FilterService.instanceFilter("imageees", "Included", "photo"));
 		assertThrows(FilterNotFoundException.class, ()-> FilterService.instanceFilter("hashtag", "Included", "AI"));
 		assertThrows(FilterNotFoundException.class, ()-> FilterService.instanceFilter("Image", "In", "photo"));
-		assertThrows(FilterNotFoundException.class, ()-> FilterService.instanceFilter("Hashtag", "Included", 1));
+		assertThrows(FilterIllegalArgumentException.class, ()-> FilterService.instanceFilter("Hashtag", "Included", 1));
 	}
 }
