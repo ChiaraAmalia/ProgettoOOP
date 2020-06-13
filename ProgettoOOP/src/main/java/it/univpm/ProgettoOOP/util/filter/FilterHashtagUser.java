6b869aa54301bcdfed29bf68d29db3a608f6808e
Piero Campitelli <S1089287@studenti.univpm.it>
2020-06-13 18:48:19 +0200

    PRoblema FilterHashtagUSer

---
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/util/filter/FilterHashtagUser.java
@@ -0,0 +1,23 @@
+package it.univpm.ProgettoOOP.util.filter;
+
+import com.fasterxml.classmate.Filter;
+
+import it.univpm.ProgettoOOP.model.Tweet;
+import it.univpm.ProgettoOOP.util.other.FilterArrayString;
+
+public class FilterHashtagUser extends FilterArrayString implements Filter {
+	public FilterHashtagUser (Object parameters) {
+		super(parameters);
+	}
+	public boolean filter (Tweet tweet) {
+		for(String p:param) {
+			for(int i=0; i<tweet.getUsers().size(); i++) {
+				if( tweet.getUsers().get(i).getScreenName().equals(p))
+					return true;
+		}
+			
+	}
+		return false;
+
+}
+}
