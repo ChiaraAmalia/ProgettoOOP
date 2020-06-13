368f8dafb27349733336b56d96ec6dba44cedb8f
Piero Campitelli <S1089287@studenti.univpm.it>
2020-06-13 14:24:00 +0200

    FilterHashtagNOTGreater

---
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/util/filter/FilterHashtagNotGreater.java
@@ -0,0 +1,23 @@
+package it.univpm.ProgettoOOP.util.filter;
+
+import it.univpm.ProgettoOOP.model.Tweet;
+import it.univpm.ProgettoOOP.util.other.Filter;
+import it.univpm.ProgettoOOP.util.other.Filter1Compare;
+
+
+public class FilterHashtagNotGreater extends Filter1Compare implements Filter { 
+
+	public FilterHashtagNotGreater(Object paramFilter) {                                  
+		super(paramFilter);
+	}
+	
+	public boolean filter (Tweet tweet) {
+		for(int i=0; i<tweet.getEntities().size(); i++) {
+			if( tweet.getEntities().get(i).getHashtags().size()< paramFilter) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
