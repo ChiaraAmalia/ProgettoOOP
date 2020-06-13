ab5a518601be7029c2493c31f79966c78f6a9744
Chiara Amalia Caporusso <s1087171@studenti.univpm.it>
2020-06-13 13:39:25 +0200

    aggiustato filterHashtagGreater

--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/util/filter/FilterHashtagGreater.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/util/filter/FilterHashtagGreater.java
@@ -2,17 +2,26 @@ package it.univpm.ProgettoOOP.util.filter;
 
 import it.univpm.ProgettoOOP.model.Tweet;
 import it.univpm.ProgettoOOP.util.other.Filter;
+import it.univpm.ProgettoOOP.util.other.Filter1Compare;
 import it.univpm.ProgettoOOP.util.other.Filter2Compare;
 
-//public class FilterHashtagGreater extends Filter2Compare implements Filter {      //Filter2Comapre è una superclasse che serve
-/*	public FilterHashtagGreater (Tweet tweet) {                                   //per inizializzare l'oggetto filtro con un numero
-		super(number);
+public class FilterHashtagGreater extends Filter1Compare implements Filter { 
+
+	public FilterHashtagGreater(Object parametroFiltro) {                                  
+		super(parametroFiltro);
 	}
 	
 	public boolean filter (Tweet tweet) {
-		for (int i=0)
-		tweet.getEntities().get(i).getHashtags().get(j).getText())
-	}*/
+		for(int i=0; i<tweet.getEntities().size(); i++) {
+			if( tweet.getEntities().get(i).getHashtags().size()> parametroFiltro) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}		
+	
 	
 
 //}
