6f2e3df192711727e6dd93201df104075dbb91ef
Chiara Amalia Caporusso <s1087171@studenti.univpm.it>
2020-06-14 19:20:29 +0200

    aggiunta di altri filtri

--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/util/filter/FilterMentionsIncluded.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/util/filter/FilterMentionsIncluded.java
@@ -4,15 +4,20 @@ import it.univpm.ProgettoOOP.model.Tweet;
 import it.univpm.ProgettoOOP.util.other.Filter;
 import it.univpm.ProgettoOOP.util.other.FilterArrayString;
 
-public class FilterInMentions extends FilterArrayString implements Filter {
-	public FilterInMentions(Object parameters) {
+public class FilterMentionsIncluded extends FilterArrayString implements Filter {
+	
+	public FilterMentionsIncluded(Object parameters) {
 		super(parameters);
 	}
+	
 	public boolean filter(Tweet tweet) {
+		for(String p :param) {
 			for(int i=0;i<tweet.getEntities().size();i++) {
-				for(int j=0; j<tweet.getEntities().get(i).getHashtags().size(); j++)
-					if(tweet.getEntities().get(i).getMentions().get(j).getScreenName()!=null)
+				for(int j=0; j<tweet.getEntities().get(i).getMentions().size(); j++) {
+					if(tweet.getEntities().get(i).getMentions().get(j).getScreenName().equals(p))
 						return true;
+				}
+			}
 		}
 		return false;
 	}
