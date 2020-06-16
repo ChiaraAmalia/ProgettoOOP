ac8348a80aa2bca65481125101f5d24645ee4b14
Chiara Amalia Caporusso <s1087171@studenti.univpm.it>
2020-06-16 11:17:46 +0200

    modifica uml

--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/model/NumeroHashtag.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/model/NumeroHashtag.java
@@ -2,76 +2,58 @@ package it.univpm.ProgettoOOP.model;
 
 import java.util.ArrayList;
 
+/**
+ * Questa classe viene utilizzata per salvare il numero di hashtag per ogni tweet con il relativo 
+ * hashtag utilizzato un numero di volte da un utente
+ * @author Chiara Amalia Caporusso
+ * @author Piero Campitelli
+ *
+ */
+
 public class NumeroHashtag {
 	
 	private int nHashtag;
-//	private ArrayList<Hashtag> hash = new ArrayList<Hashtag>();
 	private String hash;
 
-	/**
-	 * @param nHashtag
-	 */
+	
 	public NumeroHashtag(int nHashtag,String hash) {
 		super();
 		this.nHashtag = nHashtag;
 		this.hash = hash;
 	}
 
-	/**
-	 * 
-	 */
 	public NumeroHashtag() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @return the nHashtag
+	 * @return il numero di hashtag
 	 */
 	public int getnHashtag() {
 		return nHashtag;
 	}
 
 	/**
-	 * @param nHashtag the nHashtag to set
+	 * @param il numero di hashtag settato a un nuovo valore
 	 */
 	public void setnHashtag(int nHashtag) {
 		this.nHashtag = nHashtag;
 	}
 
 	/**
-	 * @return the hashtag
+	 * @return l'hashtag
 	 */
 	public String getHashtag() {
 		return hash;
 	}
 
 	/**
-	 * @param hashtag the hashtag to set
+	 * @param il valore dell'hashtag settato ad un altro valore
 	 */
 	public void setHashtag(String newHash) {
-//		hash.add(newHash);
 		this.hash = newHash;
 	}
 	
-	/**
-	 * Viene utilizzato per confrontare l'uguaglianza tra due hashtag
-	 * @param n oggetto di tipo NumeroHashtag
-	 * @return hash
-	 */
-	public boolean equals(NumeroHashtag n) {
-		if (n == this) {
-			return true;
-		}
-		if (!(n instanceof NumeroHashtag)) {
-			return false;
-		}
-		NumeroHashtag Hash = (NumeroHashtag) n;
-		return this.hash.equals(Hash.hash);
-	}
-
-	public int hashCode() {
-		return hash.hashCode();
-	}
 }
 
--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/util/stats/StatsHashtags.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/util/stats/StatsHashtags.java
@@ -9,22 +9,30 @@ import it.univpm.ProgettoOOP.service.JSONParse;
 
 public class StatsHashtags {
 	
+	/**
+	 * Questo metodo permette di filtrare i tweet in base all'user che l'ha creato.
+	 * Viene utilizzato per contare quante volte un hashtag compare nei tweet scritti dall'utente
+	 * @param tw ArrayList di Tweet filtrato
+	 * @return un ArrayList di tipo NumeroHashtag con all'interno tutti gli hashtag con il relativo conteggio
+	 */
 	public ArrayList<NumeroHashtag> HashtagTweet(ArrayList<Tweet> tw){
 		
 		ArrayList<NumeroHashtag> nHash = new ArrayList<NumeroHashtag>();
 		int z = 0;
 		NumeroHashtag a = new NumeroHashtag();
-
-		for(Tweet t:tw) {	
+		
+		for(Tweet t:tw) {
 			a.setnHashtag(0);
-				for(int i=0; i<t.getEntities().size(); i++) { //prende entità
-					for(int j=0; j<t.getEntities().get(i).getHashtags().size();j++) { //prende gli hashtag	
-						a.setHashtag(t.getEntities().get(i).getHashtags().get(j).getText());
-						if(!nHash.contains(a))
-							nHash.add(a);//prende il singolo hashtag	
+			for(int i=0; i<t.getEntities().size(); i++) { //prende entità
+				for(int j=0; j<t.getEntities().get(i).getHashtags().size();j++) { //prende gli hashtag	
+					a.setHashtag(t.getEntities().get(i).getHashtags().get(j).getText()); //prende il singolo hashtag
+					if(!nHash.contains(a)) {
+						nHash.add(a);
 					}
 				}
+			}
 		}
+
 		for(NumeroHashtag n: nHash) {
 			String b = n.getHashtag();
 			for(Tweet t: tw) {
@@ -40,6 +48,12 @@ public class StatsHashtags {
 		return nHash;		
 	}
 	
+	/**
+	 * Questo metodo permette di contare tutti gli hashtag che sono contenuti all'interno di un tweet.
+	 * Il conteggio di hashtag per ogni tweet viene poi salvato all'interno di un array che poi verrà
+	 * passato al metodo all'interno della ControllerClass
+	 * @return un array di interi contenente il numero di hashtag per ogni tweet
+	 */
 	public int[] NumHashtag() {
 		
 		ArrayList<Tweet> tw = JSONParse.ParseInformazioni();
